b821d5055cf562c9cbede1c1ffcb51a660680bb3
codingaring <sksms2104@naver.com>
2024-07-04 18:44:56 +0900

    🔧 Chore: color 팔레트 추가

---
+++ src/styles/color.js
@@ -0,0 +1,18 @@
+const COLORS = {
+  white: "#ffffff",
+  black900: "#000000",
+  black800: "#171717",
+  black700: "#333236",
+  black600: "#4b4b4b",
+  blackOverlay: "rgba(0,0,0, 0.7)",
+  gray900: "#787486",
+  gray800: "#9fa6b2",
+  gray700: "#d9d9d9",
+  gray600: "#eeeeee",
+  gray500: "#fafafa",
+  pink900: "#ff7b7b",
+  pink400: "#ffcece",
+  blue400: "#76a5ea",
+};
+
+export default COLORS;
