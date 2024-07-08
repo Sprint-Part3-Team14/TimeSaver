2b9df1e24e6d2478cd905412faf5cd8d6526a947
KKH <gyooheon@naver.com>
2024-07-08 16:41:09 +0900

    ✨ Feat: 버튼 공용 컴포넌트 생성
    버튼 컴포넌트 공용으로 생성

---
+++ src/components/Button/ButtonStyeld.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const buttonSize = {
+  large: "14rem 0",
+  normal: "7rem 0",
+  small: "6rem 0",
+};
+
+export const Container = styled.button<{
+  fontSize: "large" | "small" | "extraSmall";
+  styleVariant: "default" | "white";
+  size: "large" | "normal" | "small";
+  width: string;
+}>`
+  width: ${({ width }) => width};
+  background-color: ${({ theme, styleVariant }) =>
+    styleVariant === "white" ? theme.color.white : theme.color.pink900};
+  color: ${({ theme, styleVariant }) => (styleVariant === "white" ? theme.color.gray900 : theme.color.white)};
+  border: ${({ theme, styleVariant }) => (styleVariant === "white" ? `1px solid ${theme.color.gray700}` : "none")};
+  padding: ${({ size }) => buttonSize[size]};
+  border-radius: ${({ size }) => (size === "large" ? "8px" : "4px")};
+  cursor: pointer;
+  font-size: ${({ fontSize, theme }) => theme.fontSize[fontSize]};
+  font-weight: 500;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: ${({ theme, styleVariant }) =>
+      styleVariant === "white" ? theme.color.gray600 : theme.color.pink400};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.color.gray600};
+  }
+`;
