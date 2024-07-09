a842bf4f13ab5130fc4bd3d68d0e36d372ef88b5
KKH <gyooheon@naver.com>
2024-07-09 11:46:07 +0900

    ✨ Feat: 초대 받은 대시보드가 없을 때 보여주는 UI 생성
    초대받은 대시보드가 없을 때 보여주는 UI

---
+++ src/pages/MyDashboard/InvitationList/EmptyInvitation/EmptyInvitationStyled.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 66px 0;
+`;
+
+export const Text = styled.p`
+  padding-top: 24px;
+
+  color: var(--gray-gray_800, #9fa6b2);
+  text-align: center;
+  font-family: Inter;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
