32a1d7819d6a4da67e2523ad4ac189ce4da5138c
codingaring <sksms2104@naver.com>
2024-07-13 17:51:00 +0900

    🎨 Style: 카드 목록 타입 수정

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -13,6 +13,11 @@ export interface CardDataType {
   description: string;
   tags: string[];
   dueDate: string;
+  assignee: {
+    id: number;
+    nickname: string;
+    profileImageUrl: string;
+  };
   imageUrl: string;
   teamId: string;
   dashboardId: number;
