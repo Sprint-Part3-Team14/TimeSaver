f996fd6887d46ca72dfb8a3436c90de367e605fd
unknown <TaeUk471>
2024-07-30 16:50:54 +0900

    ✨ Feat: 이메일 및 비밀번호 관련 정규표현식 및 에러 로직 추가
    기존의 test 코드를 개량해서 제작

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -32,11 +32,43 @@ const LoginForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <AuthInput inputProps={emailInputProps} />
-      <AuthInput inputProps={passwordInputProps} />
-      <button type="submit">Login</button>
-    </form>
+    <div>
+      <h1>Sign In</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <AuthInput
+          name="email"
+          control={control}
+          label="Email"
+          type="text"
+          placeholder="Enter your email"
+          rules={{
+            required: "이메일을 입력해주세요.", // 필수 입력 필드 지정.
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "이메일 형식으로 작성해주세요.",
+            },
+          }}
+        />
+        <AuthInput
+          name="password"
+          control={control}
+          label="Password"
+          type="password"
+          placeholder="Enter your password"
+          rules={{
+            required: "비밀번호를 입력해주세요",
+            pattern: {
+              value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+              message: "비밀번호는 8자 이상이며, 영어 대/소문자와 숫자를 적어도 1개 이상 포함해야 합니다.",
+            },
+          }}
+          showPasswordToggle={true}
+          showPassword={showPassword}
+          setShowPassword={setShowPassword}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
   );
 };
 
