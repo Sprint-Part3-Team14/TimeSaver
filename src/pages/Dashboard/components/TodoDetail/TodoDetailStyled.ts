3c00efb57b6213f2317cc99f375b0e289019041f
codingaring <sksms2104@naver.com>
2024-08-07 17:31:03 +0900

    💄 Design: 할 일 상세보기 레이아웃 수정

--- src/pages/Dashboard/components/TodoDetail/TodoDetailStyled.ts
+++ src/pages/Dashboard/components/TodoDetail/TodoDetailStyled.ts
@@ -6,20 +6,35 @@ export const DetailContainer = styled.div`
   top: 0;
   right: 0;
   z-index: 20;
-  width: 50%;
+  width: 70%;
   height: 100%;
   background-color: ${theme.color.white};
-  box-shadow: 0px 8rem 2rem 0px rgba(90, 90, 90, 0.5);
+  box-shadow: 0rem 2rem 2rem 0rem rgba(90, 90, 90, 0.5);
+  display: grid;
+  grid-template-columns: 1fr 34rem;
+  grid-template-rows: 7.1rem 1fr;
+  grid-template-areas:
+    "a a"
+    "b c";
 `;
 
 export const DetailHeader = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1.5rem 3.5rem;
+  padding: 1.5rem 2.3rem;
+  grid-area: a;
+  box-sizing: border-box;
+  border-bottom: 1px solid ${theme.color.gray600};
 `;
 
-export const DetailMain = styled.div`
-  background-color: #fff;
-  padding: 1.7rem 3.5rem;
+export const DetailContent = styled.div`
+  padding: 2.3rem;
+  border-right: solid 0.1rem ${theme.color.gray600};
+  grid-area: b;
+`;
+
+export const DetailComments = styled.div`
+  padding: 2.3rem;
+  grid-area: c;
 `;
