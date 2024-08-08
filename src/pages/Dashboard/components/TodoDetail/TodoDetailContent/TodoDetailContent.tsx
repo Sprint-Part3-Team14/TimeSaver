b94059907a5e153497e1039674fda196313a03e5
codingaring <sksms2104@naver.com>
2024-08-08 17:29:00 +0900

    🎨 Style: 마감일 추가

--- src/pages/Dashboard/components/TodoDetail/TodoDetailContent/TodoDetailContent.tsx
+++ src/pages/Dashboard/components/TodoDetail/TodoDetailContent/TodoDetailContent.tsx
@@ -9,7 +9,7 @@ const TodoDetailContent = ({ todoDetailData }: { todoDetailData: DetailCard }) =
         <>뱃지</>
         <S.Assignee>작성자</S.Assignee>
       </S.Header>
-      <S.CreatedDate>{todoDetailData.createdAt}</S.CreatedDate>
+      <S.CreatedDate>{`마감일 ${todoDetailData.dueDate}`}</S.CreatedDate>
       <S.ThumbnailImage src={todoDetailData.imageUrl} alt={`${todoDetailData.title} 썸네일 이미지`} />
       <S.ContentText>{todoDetailData.description}</S.ContentText>
     </S.Container>
