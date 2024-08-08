1473a228c4aa2e2d68472d272697ed6bf7051cdc
codingaring <sksms2104@naver.com>
2024-08-08 17:01:19 +0900

    🎨 Style: 불필요한 prop drilling 제거

--- src/pages/Dashboard/components/Card/Card.tsx
+++ src/pages/Dashboard/components/Card/Card.tsx
@@ -1,26 +1,32 @@
+import useToggle from "src/hooks/useToggle";
 import CalendarTodayIcon from "../../../../components/Icons/CalendarTodayIcon";
 import formatData from "../../../../utils/formatDate";
 import { CardDataType } from "../Column/Column";
+import TodoDetail from "../TodoDetail/TodoDetail";
 import * as S from "./CardStyled";
 
-const Card = ({ card, handleOpenDetail }: { card: CardDataType; handleOpenDetail: () => void }) => {
+const Card = ({ card }: { card: CardDataType }) => {
+  const { isTrue, handleTrue, handleFalse } = useToggle();
   return (
-    <S.CardLayout onClick={handleOpenDetail}>
-      {card.imageUrl && <S.CardThumbnail src={card.imageUrl} alt={card.title} />}
-      <S.CardTitle>{card.title}</S.CardTitle>
-      <S.CardTagList>
-        {card.tags.map((tag, index) => (
-          <S.Tag key={index}>{tag}</S.Tag>
-        ))}
-      </S.CardTagList>
-      <S.CardFooter>
-        <S.CardCreatedAt>
-          <CalendarTodayIcon width={18} height={18} color={"#787486"} />
-          {formatData(card.createdAt)}
-        </S.CardCreatedAt>
-        <S.CardWriterImage src={card.assignee.profileImageUrl} alt={`${card.assignee.nickname}의 프로필 이미지`} />
-      </S.CardFooter>
-    </S.CardLayout>
+    <>
+      {isTrue && <TodoDetail handleClose={handleFalse} />}
+      <S.CardLayout onClick={handleTrue}>
+        {card.imageUrl && <S.CardThumbnail src={card.imageUrl} alt={card.title} />}
+        <S.CardTitle>{card.title}</S.CardTitle>
+        <S.CardTagList>
+          {card.tags.map((tag, index) => (
+            <S.Tag key={index}>{tag}</S.Tag>
+          ))}
+        </S.CardTagList>
+        <S.CardFooter>
+          <S.CardCreatedAt>
+            <CalendarTodayIcon width={18} height={18} color={"#787486"} />
+            {formatData(card.createdAt)}
+          </S.CardCreatedAt>
+          <S.CardWriterImage src={card.assignee.profileImageUrl} alt={`${card.assignee.nickname}의 프로필 이미지`} />
+        </S.CardFooter>
+      </S.CardLayout>
+    </>
   );
 };
 
