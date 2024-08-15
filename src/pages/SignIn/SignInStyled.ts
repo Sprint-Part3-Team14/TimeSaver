dada585ffe9e846e8353331fb67fd2b5aed8e435
TaeUk471 <asky990223@gmail.com>
2024-08-15 16:39:15 +0900

    ✨ Feat: SignIn Page 디자인 추가. 세부 조정 필요

--- src/pages/SignIn/SignInStyled.ts
+++ src/pages/SignIn/SignInStyled.ts
@@ -9,9 +9,9 @@ export const Container = styled.div`
 
   @media (max-width: 768px) {
     flex-direction: column;
-    width: 744px;
-    height: 1133px;
-    padding: 183px 112px;
+    width: 100%;
+    height: 100vh;
+    padding: 2rem 2rem;
   }
 
   @media (max-width: 480px) {
@@ -37,6 +37,8 @@ export const SignLogoBox = styled.div`
   width: 42.1rem;
   height: 41.6rem;
   background-image: url("/images/SIGN_LOGO.png");
+  background-size: cover;
+  background-position: center;
 `;
 
 export const SignInBox = styled.div`
@@ -55,14 +57,31 @@ export const SignInBox = styled.div`
   }
 
   @media (max-width: 768px) {
-    width: 744px;
-    height: 1133px;
-    padding: 183px 112px;
+    width: 100%;
+    height: 100%;
+    padding: 2rem 2rem;
   }
 
   @media (max-width: 480px) {
     width: 100%;
     height: auto;
     padding: auto 12px;
+  }
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 400px;
+  margin-top: 6rem;
+
+  @media (max-width: 768px) {
+    max-width: 100%;
+    padding: 0 24px;
+  }
+
+  @media (max-width: 480px) {
+    padding: 0;
   }
 `;
