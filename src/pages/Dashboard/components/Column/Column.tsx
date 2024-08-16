3ebc8606a47a8ad46a2f2ef226a233382eaf1ffe
codingaring <sksms2104@naver.com>
2024-08-17 00:46:46 +0900

    ✨ Feat: 담당자 목록 데이터 패칭 추가

--- src/pages/Dashboard/components/Column/Column.tsx
+++ src/pages/Dashboard/components/Column/Column.tsx
@@ -42,7 +42,7 @@ const Column = ({ columnTitle, columnId, dashboardId }: ColumnPropType) => {
 
   return (
     <>
-      {isCreateCard && <CreateCard handleClose={handleCloseCreateCard} />}
+      {isCreateCard && <CreateCard handleClose={handleCloseCreateCard} dashboardId={dashboardId} />}
       <S.DashboardColumnLayout>
         <S.ColumnHeader>
           <S.ColumnName>{columnTitle}</S.ColumnName>
