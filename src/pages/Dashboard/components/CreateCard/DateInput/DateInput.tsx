6cf35f0ac2dbab3688ee312cda295f6010175268
codingaring <sksms2104@naver.com>
2024-08-20 00:59:14 +0900

    🎨 Style: DateInput 컴포넌트에서 Container도 포함하도록 변경

--- src/pages/Dashboard/components/CreateCard/DateInput/DateInput.tsx
+++ src/pages/Dashboard/components/CreateCard/DateInput/DateInput.tsx
@@ -9,10 +9,10 @@ const DateInput = ({
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }) => {
   return (
-    <>
+    <S.DateContainer>
       <S.DateLabel htmlFor="date">마감일</S.DateLabel>
       <S.DateInputStyle required type="datetime-local" id="date" value={dateValue} onChange={onChange} />
-    </>
+    </S.DateContainer>
   );
 };
 
