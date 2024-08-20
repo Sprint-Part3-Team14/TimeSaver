776c417f52aaa824c0153ec796ae68f960cce77d
codingaring <sksms2104@naver.com>
2024-08-20 13:04:42 +0900

    💄 Design: 태그 컴포넌트 UI 구현

---
+++ src/pages/Dashboard/components/CreateCard/AddTag/AddTagStyled.ts
@@ -0,0 +1,37 @@
+import theme from "src/styles/theme";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const Title = styled.h1`
+  color: ${theme.color.black700};
+  font-size: 1.6rem;
+  font-weight: 600;
+`;
+
+export const TagInput = styled.input`
+  border: 0.1rem solid ${theme.color.gray600};
+  border-radius: 0.6rem;
+  padding: 1rem 1.5rem;
+  outline-color: ${theme.color.pink400};
+
+  &:placeholder {
+    color: ${theme.color.gray600};
+  }
+`;
+
+export const TagContainer = styled.div`
+  display: flex;
+  overflow-x: scroll;
+  overflow-y: hidden;
+  gap: 1rem;
+  text-align: center;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
