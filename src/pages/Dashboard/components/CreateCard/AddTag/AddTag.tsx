8c902c898ad78414bb4cafa4267bb4e2f1889f1d
codingaring <sksms2104@naver.com>
2024-08-20 13:34:11 +0900

    ✨ Feat: 태그 추가 기능 구현

--- src/pages/Dashboard/components/CreateCard/AddTag/AddTag.tsx
+++ src/pages/Dashboard/components/CreateCard/AddTag/AddTag.tsx
@@ -1,11 +1,28 @@
+import { KeyboardEvent } from "react";
+import useInputValue from "src/hooks/useInputValue";
 import * as S from "./AddTagStyled";
 import Tag from "./Tag/Tag";
 
-const AddTag = ({ tagList }: { tagList: string[] }) => {
+const AddTag = ({ tagList, handleAddList }: { tagList: string[]; handleAddList: (value: string) => void }) => {
+  const { value, handleChangeValue, handleResetValue } = useInputValue();
+
+  function handleEnterEvent(event: KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      handleResetValue();
+      handleAddList(value);
+    }
+  }
+
   return (
     <S.Container>
       <S.Title>태그</S.Title>
-      <S.TagInput type="input" placeholder={"입력 후 Enter"} />
+      <S.TagInput
+        type="input"
+        placeholder={"입력 후 Enter"}
+        onChange={handleChangeValue}
+        value={value}
+        onKeyDown={handleEnterEvent}
+      />
       <S.TagContainer>
         {tagList.map((tag, index) => (
           <Tag key={`${index}${tag}`} text={tag} />
