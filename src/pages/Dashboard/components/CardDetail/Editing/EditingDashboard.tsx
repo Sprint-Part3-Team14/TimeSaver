0984e0d825df09fbd781f9f5ca8aeb8dee1c2490
codingaring <sksms2104@naver.com>
2024-08-20 16:07:40 +0900

    ✨ Feat: 삭제에 성공했을 때 카드가 닫힌 이후에 invalidateQueries가 동작하도록 추가

--- src/pages/Dashboard/components/CardDetail/Editing/EditingDashboard.tsx
+++ src/pages/Dashboard/components/CardDetail/Editing/EditingDashboard.tsx
@@ -1,9 +1,36 @@
 import { useRef } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import useOutsideClick from "src/hooks/useOutsideClick";
+import { deleteCard } from "src/utils/api";
 import * as S from "./EditingDashboardStyled";
 
-const EditingDashboard = ({ handleClose }: { handleClose: () => void }) => {
+const EditingDashboard = ({
+  handleClose,
+  columnId,
+  cardId,
+  handleDetailClose,
+}: {
+  handleClose: () => void;
+  columnId: number;
+  cardId: number;
+  handleDetailClose: () => void;
+}) => {
+  const queryClient = useQueryClient();
   const EditingRef = useRef<HTMLDivElement>(null);
+
+  const cardDeleteMutation = useMutation({
+    mutationFn: async (cardId: number) => await deleteCard(cardId),
+    onSuccess: () => {
+      handleDetailClose();
+      setTimeout(() => {
+        queryClient.invalidateQueries({ queryKey: [`column-${columnId}`, "cardList"] });
+      }, 500);
+    },
+  });
+
+  function handleDeleteCard() {
+    cardDeleteMutation.mutate(cardId);
+  }
 
   useOutsideClick(EditingRef, handleClose);
 
@@ -12,7 +39,7 @@ const EditingDashboard = ({ handleClose }: { handleClose: () => void }) => {
       <S.EditingButton type="button" onClick={handleClose}>
         수정하기
       </S.EditingButton>
-      <S.EditingButton type="button" onClick={handleClose}>
+      <S.EditingButton type="button" onClick={handleDeleteCard}>
         삭제하기
       </S.EditingButton>
     </S.EditingContainer>
