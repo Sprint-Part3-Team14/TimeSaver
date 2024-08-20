876c3bc6d2b85e991de8f41a84479cc2616956dc
codingaring <sksms2104@naver.com>
2024-08-20 15:25:39 +0900

    💄 Design: 브라우저 세로 스크롤 생기지 않도록 수정

--- src/DashboardLayoutStyled.ts
+++ src/DashboardLayoutStyled.ts
@@ -1,9 +1,8 @@
 import styled from "styled-components";
 
 export const DashboardLayout = styled.div`
-  box-sizing: border-box;
-  height: 100vh;
   display: grid;
+  height: 100vh;
   grid-template-rows: 7rem 1fr;
   grid-template-columns: 30rem 1fr;
   grid-template-areas:
@@ -13,18 +12,19 @@ export const DashboardLayout = styled.div`
 
 export const HeaderLayout = styled.header`
   grid-area: a;
-  background-color: blue;
 `;
 
 export const SideBarLayout = styled.aside`
   grid-area: b;
   border-right: 0.1rem solid ${({ theme }) => theme.color.gray700};
+  height: 100%;
 `;
 
 export const ContentLayout = styled.main`
   position: relative;
   grid-area: c;
   min-width: 0;
+  height: 100%;
 `;
 
 export default DashboardLayout;
