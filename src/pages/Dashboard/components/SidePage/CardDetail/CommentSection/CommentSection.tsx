cb4917251f42f6d0b27b68a28f09a3e46b681038
codingaring <sksms2104@naver.com>
2024-08-22 01:22:52 +0900

    ♻️ Refactor: 사이드 페이지 컴포넌트로 파일 이동

--- src/pages/Dashboard/components/SidePage/CardDetail/CommentSection/CommentSection.tsx
+++ src/pages/Dashboard/components/SidePage/CardDetail/CommentSection/CommentSection.tsx
@@ -4,9 +4,9 @@ import { Comment, CreateComments } from "src/utils/apiType";
 import useInputValue from "src/hooks/useInputValue";
 import { deleteComments, postComments } from "src/utils/api";
 import Button from "src/components/Button/Button";
-import { CurrentIdListType } from "../../Card/Card";
-import EmptyComment from "./EmptyComment";
+import { CurrentIdListType } from "../../../Card/Card";
 import EditingComment from "./EditingComment/EditingComment";
+import EmptyComment from "./EmptyComment";
 import * as S from "./CommentsSectionStyled";
 
 interface EditingState {
