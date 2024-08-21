e2f555871658c2eaaa63705f316442cda90c4ee0
codingaring <sksms2104@naver.com>
2024-08-21 22:33:10 +0900

    🎨 Style: 스타일 코드를 주입시켜줄 수 있도록 prop 추가

--- src/pages/Dashboard/components/CardDetail/CardDetailContent/CardDetailContentStyled.ts
+++ src/pages/Dashboard/components/CardDetail/CardDetailContent/CardDetailContentStyled.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 import theme from "src/styles/theme";
 
+export const Container = styled.div<{ addStyle?: string }>`
+  ${({ addStyle }) => (addStyle ? addStyle : "")}
+`;
+
 export const Header = styled.div`
   display: flex;
   gap: 1rem;
