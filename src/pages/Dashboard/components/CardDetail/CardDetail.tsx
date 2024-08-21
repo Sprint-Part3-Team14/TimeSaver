8a5c72762d42997496c24f7013331ee92a517325
codingaring <sksms2104@naver.com>
2024-08-22 00:30:19 +0900

    ♻️ Refactor: SidePage 사용하여 제작

--- src/pages/Dashboard/components/CardDetail/CardDetail.tsx
+++ src/pages/Dashboard/components/CardDetail/CardDetail.tsx
@@ -1,37 +1,22 @@
-import { useRef } from "react";
 import { useQuery } from "@tanstack/react-query";
 import KebabIcon from "src/components/Icons/KebabIcon";
-import ArrowBackwardIcon from "src/components/Icons/ArrowBackwardIcon";
-import Portal from "src/components/_common/Portal";
+import { SidePage, SidePageBody, SidePageHeader } from "src/components/SidePage/SidePage";
 import { getCardInformation, getComments } from "src/utils/api";
-import { GetComments } from "src/utils/apiType";
 import useToggle from "src/hooks/useToggle";
-import useOutsideClick from "src/hooks/useOutsideClick";
 import { CurrentIdListType } from "../Card/Card";
 import TodoDetailContent from "./CardDetailContent/CardDetailContent";
 import CommentSection from "./CommentSection/CommentSection";
 import EditingDashboard from "./Editing/EditingDashboard";
 import * as S from "./CardDetailStyled";
+import type { GetComments } from "src/utils/apiType";
 
-interface TodoDetailProps {
+interface CardDetailProps {
   handleClose: () => void;
   currentIdList: CurrentIdListType;
 }
 
-const TodoDetail = ({ handleClose, currentIdList }: TodoDetailProps) => {
-  const { isTrue: isClose, handleTrue: handleAnimationClosing } = useToggle();
-  const { isTrue: isEditing, handleToggle: handleEditingToggle, handleFalse: handleEditClose } = useToggle();
-  const CardDetailRef = useRef<HTMLDivElement>(null);
-
-  useOutsideClick(CardDetailRef, handleClosing);
-
-  function handleClosing() {
-    handleAnimationClosing();
-    setTimeout(() => {
-      handleClose();
-    }, 500);
-  }
-
+const CardDetail = ({ handleClose, currentIdList }: CardDetailProps) => {
+  const { isTrue: isEditing, handleToggle: handleToggleEditing, handleFalse: handleCloseEdit } = useToggle();
   const { data: cardDetail } = useQuery({
     queryKey: ["cardDetail", currentIdList.cardId],
     queryFn: async () => {
@@ -51,35 +36,32 @@ const TodoDetail = ({ handleClose, currentIdList }: TodoDetailProps) => {
   }
 
   if (!cardComment) {
-    return <div>댓글이 없어용</div>;
+    return <div>댓글 없떠잉</div>;
   }
 
   return (
     <>
-      <Portal>
-        <S.DetailContainer isClose={isClose} ref={CardDetailRef}>
-          <S.DetailHeader>
-            <S.Button type="button" onClick={handleClosing}>
-              <ArrowBackwardIcon width={22} height={22} />
-            </S.Button>
-            <S.Button type="button" onClick={handleEditingToggle}>
-              {isEditing && (
-                <EditingDashboard
-                  handleClose={handleEditClose}
-                  columnId={currentIdList.columnId}
-                  cardId={currentIdList.cardId}
-                  handleDetailClose={handleClosing}
-                />
-              )}
-              <KebabIcon width={13} height={20} />
-            </S.Button>
-          </S.DetailHeader>
-          <TodoDetailContent todoDetailData={cardDetail} />
+      {isEditing && (
+        <EditingDashboard
+          handleClose={handleCloseEdit}
+          columnId={currentIdList.columnId}
+          cardId={currentIdList.cardId}
+          handleDetailClose={handleClose}
+        />
+      )}
+      <SidePage handleClose={handleClose}>
+        <SidePageHeader handleClosing={handleClose}>
+          <S.Button type="button" onClick={handleToggleEditing}>
+            <KebabIcon width={13} height={20} />
+          </S.Button>
+        </SidePageHeader>
+        <SidePageBody addStyle={S.cardDetailLayout}>
+          <TodoDetailContent todoDetailData={cardDetail} addStyle={S.cardContentStyle} />
           <CommentSection commentList={cardComment.comments.reverse()} currentIdList={currentIdList} />
-        </S.DetailContainer>
-      </Portal>
+        </SidePageBody>
+      </SidePage>
     </>
   );
 };
 
-export default TodoDetail;
+export default CardDetail;
