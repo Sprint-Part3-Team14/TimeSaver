892b3699cbcf40c4cbf6317859d8970080ffebad
codingaring <sksms2104@naver.com>
2024-08-21 20:42:54 +0900

    ✨ Feat: SidePage 새로 구현

--- src/components/SidePage/SidePage.tsx
+++ src/components/SidePage/SidePage.tsx
@@ -1,25 +1,34 @@
-import { ReactNode, RefObject } from "react";
+import { ReactNode, useRef } from "react";
 import Portal from "src/components/_common/Portal";
+import useToggle from "src/hooks/useToggle";
+import { Button } from "src/pages/Dashboard/components/CardDetail/CardDetailStyled";
+import ArrowBackwardIcon from "src/components/Icons/ArrowBackwardIcon";
 import useOutsideClick from "src/hooks/useOutsideClick";
 import * as S from "./SidePageStyled";
 
-export interface SidePageBaseProp {
-  isClose: boolean;
-  ref: RefObject<HTMLDivElement>;
-  handleClose: () => void;
-  children: ReactNode;
-}
+export const SidePage = ({ handleClose, children }: { handleClose: () => void; children: ReactNode }) => {
+  const { isTrue: isAnimationClose, handleTrue: handleAnimationClose } = useToggle();
+  const sidePageRef = useRef<HTMLDivElement>(null);
 
-const SidePage = ({ isClose, ref, handleClose, children }: SidePageBaseProp) => {
-  useOutsideClick(ref, handleClose);
+  useOutsideClick(sidePageRef, handleClosing);
+
+  function handleClosing() {
+    handleAnimationClose();
+    setTimeout(() => {
+      handleClose();
+    }, 500);
+  }
 
   return (
     <Portal>
-      <S.SidePageContainer isClose={isClose} ref={ref}>
-        {children}
+      <S.SidePageContainer isClose={isAnimationClose} ref={sidePageRef}>
+        <S.SidePageHeader>
+          <Button type="button" onClick={handleClosing}>
+            <ArrowBackwardIcon width={22} height={22} />
+          </Button>
+        </S.SidePageHeader>
+        <S.SidePageBody>{children}</S.SidePageBody>
       </S.SidePageContainer>
     </Portal>
   );
 };
-
-export default SidePage;
