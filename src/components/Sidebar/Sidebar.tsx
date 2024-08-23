981d2f9efa4591667964409985f6dd8d6390339b
codingaring <sksms2104@naver.com>
2024-08-23 15:59:29 +0900

    ✨ Feat: SideBar에 대시보드 목록 데이터 패칭

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,31 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { dashboardQueryKeys } from "src/queryFactory/dashboardQueryKeys";
+
 import EllipseIcon from "../Icons/EllipseIcon";
 import CrownIcon from "../Icons/CrownIcon";
 
 import SidebarTitle from "./SidebarTItle/SidebarTitle";
-
 import * as S from "./SidebarStyled";
 
-const MyDashboard = [
-  {
-    id: 0,
-    title: "테스트1",
-    color: "#7AC555",
-    createdByMe: true,
-  },
-  {
-    id: 1,
-    title: "테스트2",
-    color: "#760DDE",
-    createdByMe: false,
-  },
-];
-
 const Sidebar = () => {
+  const { data: dashboardList } = useQuery(dashboardQueryKeys.list({ navigationMethod: "pagination" }));
+
+  if (!dashboardList) {
+    return <div />;
+  }
   return (
     <>
       <SidebarTitle />
       <S.Container>
-        {MyDashboard.map(({ title, color, createdByMe, id }) => (
+        {dashboardList.dashboards.map(({ title, color, createdByMe, id }) => (
           <S.List key={id} href={`/dashboard/${id}`}>
             <EllipseIcon width={8} height={8} color={color} />
             <S.Title>
