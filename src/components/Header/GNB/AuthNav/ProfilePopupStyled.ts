b1b6b003d61557a439cd22c30bed1fc8828abf8e
TaeUk471 <asky990223@gmail.com>
2024-08-23 21:51:37 +0900

    💄 Fix: 세세한 수정 사항 및 AuthNav 코드리뷰사항 수정

--- src/components/Header/GNB/AuthNav/ProfilePopupStyled.ts
+++ src/components/Header/GNB/AuthNav/ProfilePopupStyled.ts
@@ -1,3 +1,4 @@
+import theme from "src/styles/theme";
 import styled from "styled-components";
 
 export const ProfilePopupContainer = styled.div`
@@ -10,24 +11,27 @@ export const ProfilePopupContainer = styled.div`
 `;
 
 export const PopupContent = styled.div`
-  ${props => props.theme.displays.columnCenter};
+  display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
   overflow: hidden;
   height: 6rem;
   width: 10rem;
   border-radius: 0.8rem;
-  background-color: ${props => props.theme.color.white};
+  background-color: ${theme.color.white};
   box-shadow: 0px 0.4rem 0.6rem rgba(0, 0, 0, 0.1);
 `;
 
 export const ProfilePopupButton = styled.button`
-  ${props => props.theme.displays.rowCenter};
-  font-size: ${props => props.theme.fontSize.normal};
+  ${theme.displays.rowCenter};
+  font-size: ${theme.fontSize.normal};
   font-weight: bold;
   height: 5rem;
   width: 100%;
 
   &:hover {
-    background-color: ${props => props.theme.color.gray600};
-    color: ${props => props.theme.color.black800};
+    background-color: ${theme.color.gray600};
+    color: ${theme.color.black800};
   }
 `;
--- src/components/Header/GNB/AuthNavStyled.ts
+++ src/components/Header/GNB/AuthNavStyled.ts
@@ -1,40 +1,34 @@
+import theme from "src/styles/theme";
 import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
   position: sticky;
   top: 0;
   z-index: 10;
-  display: flex;
   height: 7rem;
   width: 100%;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  border-bottom: 0.1rem solid ${props => props.theme.color.gray300};
-  background-color: ${props => props.theme.color.white};
-  padding-left: 2rem;
-  padding-right: 2rem;
+  ${props => props.theme.displays.spaceBetween};
+  border-bottom: 0.1rem solid ${theme.color.gray700};
+  background-color: ${theme.color.white};
 
-  @media (max-width: ${props => props.theme.breakpoints.mobile}) {
+  @media ${theme.device.mobile} {
     padding-left: 4rem;
     padding-right: 4rem;
   }
 
-  @media (min-width: ${props => props.theme.breakpoints.tablet}) {
+  @media ${theme.device.tablet} {
     padding-right: 8rem;
   }
 `;
 
 export const LogoAndTitleContainer = styled.div`
   width: 35%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
+  ${theme.displays.spaceBetween};
 `;
 
 export const TitleContainer = styled.div`
   display: flex;
-  font-size: 1.25rem;
+  font-size: ${theme.fontSize.large};
   font-weight: bold;
   padding-left: 1rem;
   gap: 0.5rem;
@@ -48,6 +42,7 @@ export const LogoContainer = styled.div`
 export const Logo = styled.div`
   width: 9.3rem;
   height: 4rem;
+  padding-left: 2rem;
   background-image: url("/images/Main_Logo.png");
   background-size: contain;
   background-repeat: no-repeat;
@@ -59,16 +54,39 @@ export const NavLinks = styled.div`
   align-items: center;
   gap: 0.75rem;
 
-  @media (min-width: ${props => props.theme.breakpoints.mobile}) {
+  @media ${theme.device.mobile} {
     gap: 2rem;
   }
 `;
 
 export const Text = styled.p`
-  color: ${props => props.theme.color.black700};
+  color: ${theme.color.black700};
   font-family: Inter;
-  font-size: 1.8rem;
+  font-size: ${theme.fontSize.large};
   font-weight: 700;
   line-height: 1.5;
   text-align: center;
+`;
+
+export const Crown = styled.div<{ createdByMe: boolean }>`
+  font-size: 1.25rem;
+  font-weight: bold;
+  position: relative;
+  padding-left: ${({ createdByMe }) => (createdByMe ? "2rem" : "0")}; // 왕관 위치를 고려한 padding
+
+  ${({ createdByMe }) =>
+    createdByMe &&
+    `
+      &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        top: 50%;
+        transform: translateY(-50%);
+        width: 1.7rem;
+        height: 1.4rem;
+        background-image: url("/images/Icons/crown.svg");
+        background-size: cover;
+      }
+    `}
 `;
