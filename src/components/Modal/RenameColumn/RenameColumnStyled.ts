3f551278cf6b67511ba357589cd7b8d244b623ed
codingaring <sksms2104@naver.com>
2024-08-23 12:18:32 +0900

    💄 Design: 컬럼명 변경 모달 UI 구현

---
+++ src/components/Modal/RenameColumn/RenameColumnStyled.ts
@@ -0,0 +1,23 @@
+import theme from "src/styles/theme";
+import styled from "styled-components";
+
+export const RenameForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+`;
+
+export const ButtonContainerStyled = styled.div`
+  width: 50%;
+  margin-left: auto;
+  display: flex;
+  gap: 1rem;
+`;
+
+// input 관련
+export const RenameInput = styled.input`
+  padding: 1.5rem;
+  border: 0.1rem solid ${theme.color.gray700};
+  border-radius: 0.6rem;
+  outline-color: ${theme.color.pink400};
+`;
