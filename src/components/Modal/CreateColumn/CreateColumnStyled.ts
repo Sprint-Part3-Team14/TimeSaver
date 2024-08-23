4354d4fc0c607c4895f1fad54e2a9a61dd747b34
codingaring <sksms2104@naver.com>
2024-08-23 13:14:22 +0900

    ✨ Feat: 컬럼 생성하기 기능 구현

---
+++ src/components/Modal/CreateColumn/CreateColumnStyled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import { ButtonContainerStyled as ButtonContainer, RenameForm, RenameInput } from "../RenameColumn/RenameColumnStyled";
+
+export const CreateColumnFormStyled = styled(RenameForm)``;
+
+export const CreateColumnNameInput = styled(RenameInput)``;
+
+export const ButtonContainerStyled = styled(ButtonContainer)``;
