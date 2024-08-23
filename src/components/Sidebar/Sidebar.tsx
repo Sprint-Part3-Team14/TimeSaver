17354cfb99d81b02e4f0aef9d4a442f8fff5ea7b
codingaring <sksms2104@naver.com>
2024-08-23 16:03:51 +0900

    ♻️ Refactor: createByMe에 따라서 가상 요소로 왕관 아이콘 추가되도록 변경

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,6 @@ import { useQuery } from "@tanstack/react-query";
 import { dashboardQueryKeys } from "src/queryFactory/dashboardQueryKeys";
 
 import EllipseIcon from "../Icons/EllipseIcon";
-import CrownIcon from "../Icons/CrownIcon";
 
 import SidebarTitle from "./SidebarTItle/SidebarTitle";
 import * as S from "./SidebarStyled";
@@ -21,9 +20,8 @@ const Sidebar = () => {
         {dashboardList.dashboards.map(({ title, color, createdByMe, id }) => (
           <S.List key={id} href={`/dashboard/${id}`}>
             <EllipseIcon width={8} height={8} color={color} />
-            <S.Title>
+            <S.Title createdByMe={createdByMe}>
               <S.Name>{title}</S.Name>
-              {createdByMe && <CrownIcon width={17.6} height={14} />}
             </S.Title>
           </S.List>
         ))}
--- src/components/Sidebar/SidebarStyled.ts
+++ src/components/Sidebar/SidebarStyled.ts
@@ -20,10 +20,21 @@ export const List = styled.a`
   cursor: pointer;
 `;
 
-export const Title = styled.div`
+export const Title = styled.div<{ createdByMe: boolean }>`
   display: flex;
   align-items: center;
   gap: 4px;
+
+  ${({ createdByMe }) =>
+    createdByMe
+      ? `&::after {
+    content: "";
+    width: 1.7rem;
+    height: 1.4rem;
+    background-image: url("/images/Icons/crown.svg");
+    background-size: cover;
+  }`
+      : ""}
 `;
 
 export const Name = styled.div`
