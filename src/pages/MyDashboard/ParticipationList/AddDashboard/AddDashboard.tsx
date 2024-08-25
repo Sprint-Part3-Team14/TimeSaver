921a7831c012f94fd0125c719ecbff1c69f8b098
TaeUk471 <asky990223@gmail.com>
2024-08-25 23:29:42 +0900

    🎨 Fix: 인라인 스타일 제거 및 경로 변경

--- src/pages/MyDashboard/ParticipationList/AddDashboard/AddDashboard.tsx
+++ src/pages/MyDashboard/ParticipationList/AddDashboard/AddDashboard.tsx
@@ -1,6 +1,6 @@
 import useToggle from "src/hooks/useToggle";
 import CreateDashboard from "src/components/Modal/CreateDashboard/CreateDashboard";
-import EditIcon from "../../../../components/Icons/EditIcon";
+import EditIcon from "src/components/Icons/EditIcon";
 import * as S from "./AddDashboardStyled";
 
 const AddDashboard = () => {
@@ -13,7 +13,7 @@ const AddDashboard = () => {
   return (
     <>
       {isOpenCreateDashboard && <CreateDashboard handleClose={handleCloseCreateDashboard} />}
-      <S.Container onClick={handleOpenCreateDashboard} style={{ cursor: "pointer" }}>
+      <S.Container onClick={handleOpenCreateDashboard}>
         <S.Title>새로운 대시보드</S.Title>
         <EditIcon width={16} height={16} color="#33333" />
       </S.Container>
