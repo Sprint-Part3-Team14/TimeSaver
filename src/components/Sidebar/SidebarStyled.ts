f339f4c2041af1158e544fd2270ab8e9c702b2b4
codingaring <sksms2104@naver.com>
2024-08-26 00:07:28 +0900

    🚚 Rename: mixinCSs -> mixin

--- src/components/Sidebar/SidebarStyled.ts
+++ src/components/Sidebar/SidebarStyled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { AfterCrownIcon } from "src/styles/mixinCSs";
+import { AfterCrownIcon } from "src/styles/mixin";
 
 export const Container = styled.ul`
   display: flex;
