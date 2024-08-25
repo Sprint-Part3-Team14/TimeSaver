b9b923fcb2e9054686800bdf65c6026288a6ef70
codingaring <sksms2104@naver.com>
2024-08-25 17:10:02 +0900

    💄 Design: layout 스타일 지정

--- src/components/SidePage/EditDashboard/EditDashboard.tsx
+++ src/components/SidePage/EditDashboard/EditDashboard.tsx
@@ -7,6 +7,11 @@ const EditDashboard = ({ handleClose }: { handleClose: () => void }) => {
       <SidePageHeader handleClosing={handleClose} addStyle={S.HeaderStyled}>
         <S.SidePageTitleStyled>대시보드 관리</S.SidePageTitleStyled>
       </SidePageHeader>
+      <S.EditDashboardLayout>
+        <S.EditDashboardSection>대시보드명 변경</S.EditDashboardSection>
+        <S.EditDashboardSection>구성원</S.EditDashboardSection>
+        <S.EditDashboardSection>초대 내역</S.EditDashboardSection>
+      </S.EditDashboardLayout>
     </SidePageLayout>
   );
 };
--- src/components/SidePage/EditDashboard/EditDashboardStyled.ts
+++ src/components/SidePage/EditDashboard/EditDashboardStyled.ts
@@ -1,6 +1,20 @@
 import theme from "src/styles/theme";
 import styled from "styled-components";
 
+export const EditDashboardLayout = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const EditDashboardSection = styled.div`
+  padding: 4.2rem;
+  flex-grow: 1;
+
+  &:not(last-child) {
+    border-bottom: 0.1rem solid ${theme.color.gray700};
+  }
+`;
+
 export const HeaderStyled = `
   justify-content : flex-start;
   gap : 2.4rem;
