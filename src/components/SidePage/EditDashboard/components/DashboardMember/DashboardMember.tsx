f03ceec4f4b73985e2467715a144b813fa55c7e8
TaeUk471 <asky990223@gmail.com>
2024-08-26 02:32:04 +0900

    🎨 Feat: 제작된 페이지네이션 적용

--- src/components/SidePage/EditDashboard/components/DashboardMember/DashboardMember.tsx
+++ src/components/SidePage/EditDashboard/components/DashboardMember/DashboardMember.tsx
@@ -1,10 +1,11 @@
-import { MouseEvent } from "react";
+import { useState, MouseEvent } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteMembers } from "src/utils/api";
 import { dashboardQueryKeys } from "src/queryFactory/dashboardQueryKeys";
 import UserProfile from "src/components/UserProfile/UserProfile";
 import Button from "src/components/Button/Button";
 import { GetMembersResponse } from "src/utils/apiResponseType";
+import Pagination from "src/components/PagiNation/PagiNation";
 import * as S from "./DashboardMemberStyled";
 
 const DashboardMember = ({
@@ -14,29 +15,48 @@ const DashboardMember = ({
   dashboardMemberList: GetMembersResponse;
   dashboardId: number;
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageSize = 4;
+
   const queryClient = useQueryClient();
-  const dashboardQuery = dashboardQueryKeys.members({ dashboardId: dashboardId, size: 4, page: 1 });
+
+  // 멤버 삭제 처리 로직
   const DeleteMemberMutation = useMutation({
     mutationFn: async (memberId: number) => await deleteMembers(memberId),
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: dashboardQuery.queryKey });
+      queryClient.invalidateQueries({
+        queryKey: dashboardQueryKeys.members({ dashboardId, size: pageSize, page: currentPage }).queryKey,
+      });
     },
   });
 
   function handleDeleteMember(event: MouseEvent<HTMLButtonElement>) {
     DeleteMemberMutation.mutate(Number(event.currentTarget.value));
   }
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+  };
+
+  // 페이지별로 데이터 필터링
+  const startIdx = (currentPage - 1) * pageSize;
+  const paginatedMembers = dashboardMemberList.members.slice(startIdx, startIdx + pageSize);
+
   return (
     <S.Container>
       <S.HeaderLayout>
         <S.Title>구성원</S.Title>
-        <div>페이지 네이션 버튼</div>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={Math.ceil((dashboardMemberList.totalCount || 0) / pageSize)}
+          onPageChange={handlePageChange}
+        />
       </S.HeaderLayout>
       <S.BodyLayout>
         <S.GridText>이름</S.GridText>
         <S.MemberContainer>
-          {dashboardMemberList.members.map((member, index) => (
-            <S.MemberOne>
+          {paginatedMembers.map((member, index) => (
+            <S.MemberOne key={member.id}>
               <UserProfile profileImageUrl={member.profileImageUrl} nickName={member.nickname} />
               {index !== 0 && (
                 <Button
