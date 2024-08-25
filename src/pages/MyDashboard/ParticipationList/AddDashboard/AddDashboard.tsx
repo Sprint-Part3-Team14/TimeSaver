30368e2a5c93137feb9cd29eff02870e3b4d7431
TaeUk471 <asky990223@gmail.com>
2024-08-25 19:18:03 +0900

    🎨 Feat: 기존 Modal my-Dashborad 페이지에 연결.

--- src/pages/MyDashboard/ParticipationList/AddDashboard/AddDashboard.tsx
+++ src/pages/MyDashboard/ParticipationList/AddDashboard/AddDashboard.tsx
@@ -1,12 +1,16 @@
+import useToggle from "src/hooks/useToggle";
+import CreateDashboard from "src/components/Modal/CreateDashboard/CreateDashboard";
 import EditIcon from "../../../../components/Icons/EditIcon";
 import * as S from "./AddDashboardStyled";
 
 const AddDashboard = () => {
+  const { isTrue: isModalOpen, handleTrue: handleOpenModal, handleFalse: handleCloseModal } = useToggle();
+
   return (
-    <S.Container>
+    <S.Container onClick={handleOpenModal} style={{ cursor: "pointer" }}>
       <S.Title>새로운 대시보드</S.Title>
       <EditIcon width={16} height={16} color="#33333" />
-      {/*아이콘 변경예정*/}
+      {isModalOpen && <CreateDashboard handleClose={handleCloseModal} />}
     </S.Container>
   );
 };
