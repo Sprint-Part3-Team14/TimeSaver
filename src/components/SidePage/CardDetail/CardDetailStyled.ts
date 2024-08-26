a40ea55dcd92ae0218af2f6ae337c66e64417c92
codingaring <sksms2104@naver.com>
2024-08-26 23:23:10 +0900

    💄 Design: 할 일 상세보기 반응형 구현

--- src/components/SidePage/CardDetail/CardDetailStyled.ts
+++ src/components/SidePage/CardDetail/CardDetailStyled.ts
@@ -5,11 +5,29 @@ export const cardDetailLayout = `
 display: grid;
 grid-template-columns: 60% 40%;
 padding : 0;
+
+@media ${theme.device.tablet}{
+  display : flex;
+  flex-direction : column;
+}
+
+@media ${theme.device.mobile}{
+  display : flex;
+  flex-direction : column;
+}
 `;
 
-export const cardContentStyle = `
+export const CardContentStyle = `
   padding : 2.3rem;
   border-right : 0.1rem solid ${theme.color.gray600};
+  
+  @media ${theme.device.tablet}{
+    border-bottom : 0.1rem solid ${theme.color.gray600};
+  }
+
+  @media ${theme.device.mobile}{
+    border-bottom : 0.1rem solid ${theme.color.gray600};
+  }
 `;
 
 export const Button = styled.button`
