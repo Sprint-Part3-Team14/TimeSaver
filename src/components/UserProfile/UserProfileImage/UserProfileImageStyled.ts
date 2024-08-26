dffd99a519d1cf6db1db8c34fa9fed8b49b4ff1d
codingaring <sksms2104@naver.com>
2024-08-26 23:26:24 +0900

    💄 Design: 프로핇 배경이 흰색일 때를 고려하여 border 추가

--- src/components/UserProfile/UserProfileImage/UserProfileImageStyled.ts
+++ src/components/UserProfile/UserProfileImage/UserProfileImageStyled.ts
@@ -1,8 +1,10 @@
+import theme from "src/styles/theme";
 import styled from "styled-components";
 
 export const UserProfileImg = styled.img<{ size?: string; addStyle?: string }>`
   border-radius: 50%;
   object-fit: cover;
+  border: 0.1rem solid ${theme.color.gray700};
   width: ${({ size }) => size};
   height: ${({ size }) => size};
   ${({ addStyle }) => addStyle}
