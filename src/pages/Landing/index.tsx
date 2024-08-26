d1b8195772dbe042695369fe68751f2dfb0e7726
TaeUk471 <asky990223@gmail.com>
2024-08-26 16:22:03 +0900

    🎨 Feat: MainPage 초안 완성.

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,34 +1,51 @@
-"use client";
-import { useQuery } from "@tanstack/react-query";
-import EditAccount from "src/components/SidePage/EditAccount/EditAcount";
-import useToggle from "src/hooks/useToggle";
-import { userQueryKeys } from "src/queryFactory/userQueryKeys";
-import { logout, postAuthLogin } from "src/utils/api";
-import { SignIn } from "src/utils/apiType";
+import React, { useState } from "react";
+import { COLOR_LIST } from "src/components/Modal/CreateDashboard/ColorConstant";
+import OptionalHeader from "src/components/Header/OptionalHeader";
+import * as S from "src/pages/Landing/LandingStyled";
 
-const Page = () => {
-  const loginTest = async () => {
-    const body: SignIn = {
-      email: "test@naver.com",
-      password: "qwer1234",
-    };
-    const res = await postAuthLogin(body);
-    console.log(res);
+const MainPage = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const images = [
+    "/images/landing1.jpg",
+    "/images/landing2.jpg",
+    "/images/landing3.jpg",
+    "/images/landing4.jpg",
+    "/images/landing5.jpg",
+  ];
+
+  const handlePrevSlide = () => {
+    setCurrentSlide(prev => (prev === 0 ? images.length - 1 : prev - 1));
   };
-  postAuthLogin({ email: "test@codeit.com", password: "sprint101" });
-  const { isTrue, handleTrue, handleFalse } = useToggle();
-  const { data: currentUser } = useQuery(userQueryKeys.current());
+
+  const handleNextSlide = () => {
+    setCurrentSlide(prev => (prev === images.length - 1 ? 0 : prev + 1));
+  };
 
   return (
-    <>
-      {isTrue && <EditAccount handleClose={handleFalse} userProfileData={currentUser} />}
-      <button type="button" onClick={handleTrue}>
-        등장
-      </button>
-      <div onClick={loginTest}>로그인</div>
-      <div onClick={logout}>로그아웃</div>
-    </>
+    <S.Container>
+      <OptionalHeader />
+      <S.MainContent>
+        <S.HeroSection>
+          <S.SlideShow>
+            <S.SlideWrapper>
+              <S.SlideImage src={images[currentSlide]} alt="Landing Slide" />
+            </S.SlideWrapper>
+            <S.PrevButton onClick={handlePrevSlide}>&lt;</S.PrevButton>
+            <S.NextButton onClick={handleNextSlide}>&gt;</S.NextButton>
+          </S.SlideShow>
+          <S.HeroText>당신의 일정을 관리하세요!</S.HeroText>
+        </S.HeroSection>
+        <S.ColorSection>
+          <S.ColorText>여러가지 색으로 일들을 그룹지어 관리하세요!</S.ColorText>
+          <S.ColorList>
+            {COLOR_LIST.map((color, index) => (
+              <S.ColorCircle key={index} color={color} />
+            ))}
+          </S.ColorList>
+        </S.ColorSection>
+      </S.MainContent>
+    </S.Container>
   );
 };
 
-export default Page;
+export default MainPage;
