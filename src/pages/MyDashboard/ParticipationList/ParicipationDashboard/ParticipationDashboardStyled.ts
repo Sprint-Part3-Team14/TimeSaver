edeacb500de1de694e30b551cd50ebea613aca7a
codingaring <sksms2104@naver.com>
2024-08-26 21:27:30 +0900

    💄 Design: 나의 대시보드 페이지에서의 대시보드 목록 반응형 수정

--- src/pages/MyDashboard/ParticipationList/ParicipationDashboard/ParticipationDashboardStyled.ts
+++ src/pages/MyDashboard/ParticipationList/ParicipationDashboard/ParticipationDashboardStyled.ts
@@ -1,28 +1,45 @@
-import { Title, TitleText } from "src/components/Sidebar/SidebarStyled";
-import theme from "src/styles/theme";
 import styled from "styled-components";
+import { AfterCrownIcon } from "src/styles/mixin";
+import theme from "src/styles/theme";
 
-export const TitleButton = styled(Title)`
+export const TitleButton = styled.a<{ createdByMe: boolean; color: string }>`
   border: 0.1rem solid ${theme.color.gray700};
   border-radius: 0.8rem;
   background-color: ${theme.color.white};
-  flex-shrink: 1;
-  padding-top: 2.5rem;
-  padding-bottom: 2.5rem;
+  padding: 2.5rem;
 
-  @media ${theme.device.tablet} {
-    &::after {
-      display: inline-block;
-    }
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+
+  &::before {
+    content: "";
+    width: 0.8rem;
+    height: 0.8rem;
+    background-color: ${({ color }) => color};
+    border-radius: 50%;
+    margin-right: 1.5rem;
+    flex-shrink: 0;
+  }
+
+  ${({ createdByMe }) => (createdByMe ? AfterCrownIcon : "")}
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.gray500};
   }
 `;
 
-export const TitleStyle = styled(TitleText)`
+export const TitleStyle = styled.p`
   color: ${theme.color.black700};
-  min-width: auto;
-  max-width: calc(100% - 10rem);
+  font-weight: 600;
+  font-size: 1.6rem;
 
-  @media ${theme.device.tablet} {
-    max-width: 70%;
+  text-wrap: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  margin-right: auto;
+
+  @media ${theme.device.mobile} {
+    max-width: 14.4rem;
   }
 `;
