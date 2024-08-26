9bad3420a855f2d274956887bf39e0f1e00c4aca
codingaring <sksms2104@naver.com>
2024-08-26 22:59:51 +0900

    💄 Design: 카드 반응형 구현

--- src/components/UserProfile/UserProfileImage/UserProfileImage.tsx
+++ src/components/UserProfile/UserProfileImage/UserProfileImage.tsx
@@ -1,12 +1,21 @@
 import { DEFAULT_PROFILEIMAGE_URL } from "src/constant/constant";
 import * as S from "./UserProfileImageStyled";
 
-const UserProfileImage = ({ profileImageUrl, size = "2.6rem" }: { profileImageUrl: string | null; size?: string }) => {
+const UserProfileImage = ({
+  profileImageUrl,
+  size = "2.6rem",
+  addStyle,
+}: {
+  profileImageUrl: string | null;
+  size?: string;
+  addStyle?: string;
+}) => {
   return (
     <S.UserProfileImg
       alt="프로필 이미지"
       src={profileImageUrl ? profileImageUrl : DEFAULT_PROFILEIMAGE_URL}
       size={size}
+      addStyle={addStyle}
     />
   );
 };
