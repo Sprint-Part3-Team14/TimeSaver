7935878c44ab4b0df7d7e4b5c62c7186818e197d
codingaring <sksms2104@naver.com>
2024-08-26 23:18:05 +0900

    ♻️ Refactor: 댓글 리스트 Response 타입 명시 후 QueryKeyFactory 적용

--- src/components/SidePage/CardDetail/CardDetail.tsx
+++ src/components/SidePage/CardDetail/CardDetail.tsx
@@ -1,15 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import KebabIcon from "src/components/Icons/KebabIcon";
 import { SidePageLayout, SidePageBody, SidePageHeader } from "src/components/SidePage/SidePage";
-import { getCardInformation, getComments } from "src/utils/api";
 import useToggle from "src/hooks/useToggle";
 import EditingDashboard from "src/components/SidePage/CardDetail/EditCard/EditingDashboard";
 import CommentSection from "src/components/SidePage/CardDetail/CommentSection/CommentSection";
-import { CurrentIdListType } from "../../../pages/Dashboard/components/Card/Card";
-import CreateCard from "../../../pages/Dashboard/components/CreateCard/CreateCard";
+import { CurrentIdListType } from "src/pages/Dashboard/components/Card/Card";
+import CreateCard from "src/pages/Dashboard/components/CreateCard/CreateCard";
+import { cardQueryKeys } from "src/queryFactory/cardQueryKeys";
+import { commentQueryKeys } from "src/queryFactory/commentsQueryKeys";
 import TodoDetailContent from "./CardDetailContent/CardDetailContent";
 import * as S from "./CardDetailStyled";
-import type { DetailCard, GetComments } from "src/utils/apiType";
 
 interface CardDetailProps {
   handleClose: () => void;
@@ -19,19 +19,11 @@ interface CardDetailProps {
 const CardDetail = ({ handleClose, currentIdList }: CardDetailProps) => {
   const { isTrue: isEditing, handleTrue: handleStartEdit, handleFalse: handleCloseEdit } = useToggle();
   const { isTrue: isOpenMenu, handleToggle: handleToggleMenu, handleFalse: handleCloseMenu } = useToggle();
-  const { data: cardDetail } = useQuery<DetailCard>({
-    queryKey: ["cardDetail", currentIdList.cardId],
-    queryFn: async () => {
-      return getCardInformation(currentIdList.cardId);
-    },
-  });
+  const { data: cardDetail } = useQuery(cardQueryKeys.detail(currentIdList.cardId));
 
-  const { data: cardComment } = useQuery<GetComments>({
-    queryKey: ["comments", currentIdList.cardId],
-    queryFn: async () => {
-      return getComments({ size: 10, cardId: currentIdList.cardId });
-    },
-  });
+  const { data: cardComment } = useQuery(
+    commentQueryKeys.list(currentIdList.cardId, { size: 10, cardId: currentIdList.cardId })
+  );
 
   if (!cardDetail) {
     return <div>데이터가 없어용</div>;
--- src/queryFactory/cardQueryKeys.ts
+++ src/queryFactory/cardQueryKeys.ts
@@ -1,6 +1,6 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 import { getCardInformation, getCardList } from "src/utils/api";
-import { CardListSearch } from "src/utils/apiType";
+import type { CardListSearch, DetailCard } from "src/utils/apiType";
 
 export const cardQueryKeys = createQueryKeys("card", {
   list: (columnId: number, props: CardListSearch) => ({
@@ -9,6 +9,6 @@ export const cardQueryKeys = createQueryKeys("card", {
   }),
   detail: (cardId: number) => ({
     queryKey: ["cardDetail", cardId],
-    queryFn: async () => await getCardInformation(cardId),
+    queryFn: async (): Promise<DetailCard> => await getCardInformation(cardId),
   }),
 });
