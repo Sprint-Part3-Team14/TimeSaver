30e34a4efb345f1b0e70da69f9fe4d2e6d67e8e7
codingaring <sksms2104@naver.com>
2024-08-27 14:02:08 +0900

    🎨Style: prop으로 img태그의 속성 상속 받도록 타입 수정

--- src/components/UserProfile/UserProfileImage/UserProfileImage.tsx
+++ src/components/UserProfile/UserProfileImage/UserProfileImage.tsx
@@ -1,15 +1,14 @@
+import { ImgHTMLAttributes } from "react";
 import { DEFAULT_PROFILEIMAGE_URL } from "src/constant/constant";
 import * as S from "./UserProfileImageStyled";
 
-const UserProfileImage = ({
-  profileImageUrl,
-  size = "2.6rem",
-  addStyle,
-}: {
+interface I_UserProfileImageProp extends ImgHTMLAttributes<HTMLImageElement> {
   profileImageUrl: string | null;
   size?: string;
   addStyle?: string;
-}) => {
+}
+
+const UserProfileImage = ({ profileImageUrl, size = "2.6rem", addStyle }: I_UserProfileImageProp) => {
   return (
     <S.UserProfileImg
       alt="프로필 이미지"
