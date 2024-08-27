bbd3da0be33bfa5d1d6d09cc396551a24cb21a78
codingaring <sksms2104@naver.com>
2024-08-27 11:48:47 +0900

    🎨 Style: 오타 수정 및 return 문 정리

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -2,35 +2,18 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import Button from "src/components/Button/Button";
 import PlusIcon from "src/components/Icons/PlusIcon";
-import CreateColumn from "src/components/Modal/CreateColumn/CretaeColumn";
+import CreateColumn from "src/components/Modal/CreateColumn/CreateColumn";
 import useToggle from "src/hooks/useToggle";
 import theme from "src/styles/theme";
 import { columnQueryKeys } from "src/queryFactory/columnQueryKeys";
-import * as S from "./DashboardStyled";
 import Column from "./components/Column/Column";
-
-export interface ColumnDataType {
-  result: "SUCCESS";
-  data: ColumnOneType[];
-}
-
-export interface ColumnOneType {
-  id: number;
-  title: string;
-  teamId: string;
-  dashboardId: number;
-  createdAt: string;
-  updatedAt: string;
-}
+import * as S from "./DashboardStyled";
+import type { ColumnResponse } from "src/utils/apiResponseType";
 
 const Dashboard = () => {
   const { id: dashboardId } = useParams();
 
-  const {
-    isTrue: isOpenCreateColumn,
-    handleTrue: handleOpenCreateColumn,
-    handleFalse: handleCloseCreateColumn,
-  } = useToggle();
+  const { isTrue: isOpenCreate, handleTrue: handleOpenCreate, handleFalse: handleCloseCreate } = useToggle();
 
   const {
     data: columnListData,
@@ -41,27 +24,19 @@ const Dashboard = () => {
     enabled: !!dashboardId,
   });
 
-  if (isLoading) {
-    return <div>로딩 중...</div>;
-  }
-
-  if (error) {
-    return <div>데이터를 불러오는 중 오류가 발생했습니다: {error.message}</div>;
-  }
-
-  if (!columnListData || !columnListData.data) {
-    return <div>데이터가 없습니다</div>;
-  }
+  if (isLoading) return <div>로딩 중...</div>;
+  if (error) return <div>데이터를 불러오는 중 오류가 발생했습니다: {error.message}</div>;
+  if (!columnListData || !columnListData.data) return <div>데이터가 없습니다</div>;
 
   return (
     <>
-      {isOpenCreateColumn && <CreateColumn handleClose={handleCloseCreateColumn} dashboardId={Number(dashboardId)} />}
+      {isOpenCreate && <CreateColumn handleClose={handleOpenCreate} dashboardId={Number(dashboardId)} />}
       <S.DashboardLayout>
-        {columnListData.data.map((column: ColumnOneType) => (
+        {columnListData.data.map((column: ColumnResponse) => (
           <Column key={column.id} columnTitle={column.title} columnId={column.id} dashboardId={Number(dashboardId)} />
         ))}
         {columnListData.data.length < 10 && (
-          <Button styleVariant="white" exceptionStyle={S.AddColumnButtonStyled} onClick={handleOpenCreateColumn}>
+          <Button styleVariant="white" exceptionStyle={S.AddColumnButtonStyled} onClick={handleCloseCreate}>
             새로운 컬럼 추가하기 <PlusIcon width={22} height={22} color={theme.color.pink900} />
           </Button>
         )}
