ea0cfc9ab8dad4f16a37ddfea0f632613c0f6504
codingaring <sksms2104@naver.com>
2024-08-27 10:57:54 +0900

    ♻️ Refactor: 애니메이션 파일 분리

--- src/components/SidePage/SidePageStyled.ts
+++ src/components/SidePage/SidePageStyled.ts
@@ -1,26 +1,7 @@
+import { slideIn, slideOut } from "src/styles/animation";
 import { ScrollCustom } from "src/styles/mixin";
 import theme from "src/styles/theme";
-import styled, { keyframes } from "styled-components";
-
-// 슬라이드 인 애니메이션
-export const slideIn = keyframes`
-  from {
-    transform: translateX(100%);
-  }
-  to {
-    transform: translateX(0);
-  }
-`;
-
-// 슬라이드 아웃 애니메이션
-export const slideOut = keyframes`
-  from {
-    transform: translateX(0);
-  }
-  to {
-    transform: translateX(100%);
-  }
-`;
+import styled from "styled-components";
 
 export const SidePageContainer = styled.div<{ isClose: boolean; addStyle?: string }>`
   position: fixed;
