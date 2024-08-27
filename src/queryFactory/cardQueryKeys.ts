f0e871b080d406088f07146f2dcba6398b02468b
codingaring <sksms2104@naver.com>
2024-08-27 11:36:19 +0900

    ♻️ Refactor: 컬럼 리스트 조회 query-key-factory 적용

--- src/queryFactory/cardQueryKeys.ts
+++ src/queryFactory/cardQueryKeys.ts
@@ -1,11 +1,12 @@
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 import { getCardInformation, getCardList } from "src/utils/api";
+import { ColumnDataType } from "src/utils/apiResponseType";
 import type { CardListSearch, DetailCard } from "src/utils/apiType";
 
 export const cardQueryKeys = createQueryKeys("card", {
   list: (columnId: number, props: CardListSearch) => ({
     queryKey: ["cardList", columnId],
-    queryFn: async () => await getCardList(props),
+    queryFn: async (): Promise<ColumnDataType> => await getCardList(props),
   }),
   detail: (cardId: number) => ({
     queryKey: ["cardDetail", cardId],
