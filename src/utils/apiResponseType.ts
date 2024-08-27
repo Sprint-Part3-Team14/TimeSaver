4208b51158130279509f4187774eb0476d637caf
codingaring <sksms2104@naver.com>
2024-08-27 11:41:31 +0900

    🎨 Style: Type보기 좋게 수정

--- src/utils/apiResponseType.ts
+++ src/utils/apiResponseType.ts
@@ -1,21 +1,41 @@
 import { DetailCard } from "./apiType";
 
-// 대시보드 멤버 조회하기
-export interface GetMembersResponse {
-  members: MembersData[];
-  totalCount: number;
+/**
+ 회원 관련
+ */
+//
+
+// 유저 정보
+export interface InviterResponse {
+  nickname: string;
+  email: string;
+  id: number;
 }
 
-export interface MembersData {
+// 프로필 이미지 업로드
+export interface ProfileImageUploadResponse {
+  profileImageUrl: string;
+}
+
+// 로그인
+export interface LoginResponse {
+  accessToken: string;
+  user: MyProfileResponse;
+}
+
+// 내 정보 조회
+export interface MyProfileResponse {
   id: number;
   email: string;
   nickname: string;
   profileImageUrl: string;
   createdAt: string;
   updatedAt: string;
-  isOwner: boolean;
-  userId: number;
 }
+
+/**
+ Cards 카드 타입
+ */
 
 // 카드 리스트 조회
 export interface ColumnDataType {
@@ -23,6 +43,33 @@ export interface ColumnDataType {
   totalCount: number;
   cursorId: null;
 }
+
+/**
+ Columns 칼럼 타입
+ */
+
+// 컬럼 리스트
+export interface ColumnListResponse {
+  result: string;
+  data: ColumnResponse[];
+}
+
+export interface ColumnResponse {
+  id: number;
+  title: string;
+  teamId: string;
+  dashboardId: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+/**
+ Comments 댓글 타입
+ */
+
+/**
+ Dashboards 대시보드 타입
+ */
 
 // 대시보드 목록 조회
 export interface DashboardListResponse {
@@ -41,24 +88,24 @@ export interface DashboardInfoData {
   userId: number;
 }
 
-// 내가 받은 초대 목록 조회
-export interface MyInvitationListResponse {
-  cursorId: number | null;
-  invitations: MyInvitationResponse[];
+// 대시보드 멤버 조회하기
+export interface GetMembersResponse {
+  members: MembersData[];
+  totalCount: number;
 }
 
-export interface MyInvitationResponse {
+export interface MembersData {
   id: number;
-  inviter: InviterResponse;
-}
-
-export interface InviterResponse {
-  nickname: string;
   email: string;
-  id: number;
+  nickname: string;
+  profileImageUrl: string;
+  createdAt: string;
+  updatedAt: string;
+  isOwner: boolean;
+  userId: number;
 }
 
-// 초대 내역 조회
+// 대시보드 초대 내역 조회
 export interface InvitationRecordResponse {
   invitations: InvitationType[];
   totalCount: number;
@@ -69,38 +116,17 @@ export interface InvitationType {
   inviter: InviterResponse;
 }
 
-// 내 정보 조회
-export interface MyProfileResponse {
+/**
+ * Invitations 초대 타입
+ */
+
+// 내가 받은 초대 목록 조회
+export interface MyInvitationListResponse {
+  cursorId: number | null;
+  invitations: MyInvitationResponse[];
+}
+
+export interface MyInvitationResponse {
   id: number;
-  email: string;
-  nickname: string;
-  profileImageUrl: string;
-  createdAt: string;
-  updatedAt: string;
-}
-
-// 프로필 이미지 업로드
-export interface ProfileImageUploadResponse {
-  profileImageUrl: string;
-}
-
-// 로그인
-export interface LoginResponse {
-  accessToken: string;
-  user: MyProfileResponse;
-}
-
-// 컬럼 리스트
-export interface ColumnListResponse {
-  result: string;
-  data: ColumnResponse[];
-}
-
-export interface ColumnResponse {
-  id: number;
-  title: string;
-  teamId: string;
-  dashboardId: number;
-  createdAt: string;
-  updatedAt: string;
+  inviter: InviterResponse;
 }
