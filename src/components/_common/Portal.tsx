38fc05ee35d69509c85adac2e76178931f59a855
codingaring <sksms2104@naver.com>
2024-08-30 00:04:01 +0900

    🐛 Fix: no-null eslint에 걸리지 않도록 조건문 추가

--- src/components/_common/Portal.tsx
+++ src/components/_common/Portal.tsx
@@ -3,7 +3,12 @@ import ReactDOM from "react-dom";
 
 const Portal = ({ children }: PropsWithChildren) => {
   const portalElement = document.getElementById("portal");
-  return ReactDOM.createPortal(<>{children}</>, portalElement!);
+
+  if (!portalElement) {
+    throw new Error("ID가 'portal'인 포털 엘리먼트를 찾을 수 없습니다.");
+  }
+
+  return ReactDOM.createPortal(<>{children}</>, portalElement);
 };
 
 export default Portal;
