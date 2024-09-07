6a00cb0dd59ff57b5b7ac352387544a1cef7877a
codingaring <sksms2104@naver.com>
2024-09-08 00:13:31 +0900

    ✨ feat: 랜딩 페이지 구현

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,27 +1,39 @@
+import { useNavigate } from "react-router-dom";
 import Button from "src/components/Button/Button";
 import OptionalHeader from "src/components/Header/OptionalHeader";
+import { getCookie } from "src/utils/CookieSetting";
 import * as S from "src/pages/Landing/LandingStyled";
 import { LandingSection, SectionType } from "./constant/landingSection";
 
 const Landing = () => {
+  const navigate = useNavigate();
+  const accessToken = getCookie("accessToken");
+
+  function handleService() {
+    if (accessToken) {
+      navigate("/my-dashboard");
+    } else {
+      navigate("/signin");
+    }
+  }
   return (
     <S.Container>
       <OptionalHeader />
       <S.BodyContainer>
         <S.HeroContainer>
-          <S.HeroText>
-            당신의 일정을 관리하고 <br /> 공유 하세요 !
-          </S.HeroText>
+          <S.HeroText>당신의 일정을 관리하고 공유 하세요 !</S.HeroText>
           <S.SubText>새로운 일정관리 서비스, 타임 세이버를 통해 일정을 관리해보세요</S.SubText>
-          <Button exceptionStyle={S.ButtonStyle}>시작하기</Button>
+          <Button onClick={handleService} exceptionStyle={S.ButtonStyle}>
+            시작하기
+          </Button>
         </S.HeroContainer>
-        {LandingSection.map((section: SectionType) => {
+        {LandingSection.map((section: SectionType, index) => {
           return (
             <S.Section key={section.id}>
               <S.SectionImg src={section.imgUrl} alt={section.alt} />
               <S.SectionDescription>
-                <S.SectionTitle>{section.title}</S.SectionTitle>
-                <S.SectionSubTitle>{section.subTitle}</S.SectionSubTitle>
+                <S.SectionTitle isOdd={index + 1 / 2 === 0}>{section.title}</S.SectionTitle>
+                <S.SectionSubTitle isOdd={index + 1 / 2 === 0}>{section.subTitle}</S.SectionSubTitle>
               </S.SectionDescription>
             </S.Section>
           );
