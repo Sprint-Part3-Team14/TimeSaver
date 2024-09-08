4ed6d0413d738140a32b1d12719e68a416f06da5
codingaring <sksms2104@naver.com>
2024-09-08 21:32:48 +0900

    ♻️ Refactor: helmet을 이용한  title 설정

--- src/pages/Notfound/index.tsx
+++ src/pages/Notfound/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 import MainLogoIcon from "src/components/Icons/MainLogoIcon";
 import Button from "src/components/Button/Button";
 import * as S from "./NotfoundStyled";
@@ -6,23 +7,28 @@ import * as S from "./NotfoundStyled";
 const Notfound = () => {
   const navigation = useNavigate();
   return (
-    <S.Background>
-      <S.Layout>
-        <MainLogoIcon width={590} height={458} />
-        <S.Text>
-          <p>oops !</p>
-          <p>PAGE NOT FOUND</p>
-        </S.Text>
-        <Button
-          onClick={() => {
-            navigation("/");
-          }}
-          styleVariant="white"
-          exceptionStyle={S.AddButtonStyle}>
-          메인으로 가기
-        </Button>
-      </S.Layout>
-    </S.Background>
+    <>
+      <Helmet>
+        <title>Page not found</title>
+      </Helmet>
+      <S.Background>
+        <S.Layout>
+          <MainLogoIcon width={590} height={458} />
+          <S.Text>
+            <p>oops !</p>
+            <p>PAGE NOT FOUND</p>
+          </S.Text>
+          <Button
+            onClick={() => {
+              navigation("/");
+            }}
+            styleVariant="white"
+            exceptionStyle={S.AddButtonStyle}>
+            메인으로 가기
+          </Button>
+        </S.Layout>
+      </S.Background>
+    </>
   );
 };
 export default Notfound;
