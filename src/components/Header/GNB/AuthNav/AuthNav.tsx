4a671cd82b581455ed499f15c3d224630a227ab3
codingaring <sksms2104@naver.com>
2024-10-01 21:11:12 +0900

    🐛 fix: 대시보드 편집 및 대시보드 삭제 기능 정상화

--- src/components/Header/GNB/AuthNav/AuthNav.tsx
+++ src/components/Header/GNB/AuthNav/AuthNav.tsx
@@ -1,49 +1,25 @@
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
-import { getDashboardDetails, getMembers } from "src/utils/api";
 import { userQueryKeys } from "src/queryFactory/userQueryKeys";
 import UserProfileImage from "src/components/UserProfile/UserProfileImage/UserProfileImage";
 import useToggle from "src/hooks/useToggle";
 import { HeaderContainer, LinkStyle, Logo } from "src/components/Header/HeaderCommonStyle";
+import { dashboardQueryKeys } from "src/queryFactory/dashboardQueryKeys";
 import DashboardInfo from "./components/DashboardInfo";
 import ProfilePopup from "./components/ProfilePopup";
 import * as S from "./AuthNavStyled";
-import type { DashboardInfoData, GetMembersResponse } from "src/utils/apiResponseType";
+
+const DEFAULT_TITLE = "";
 
 const AuthNav = ({ isLanding }: { isLanding: boolean }) => {
   const { id } = useParams<{ id: string }>();
-  const [dashboardInfo, setDashboardInfo] = useState<DashboardInfoData | null>(null);
-  const [memberList, setMemberList] = useState<GetMembersResponse | null>(null);
   const { isTrue: isOpenPopup, handleToggle } = useToggle();
-
   const { data: currentUser } = useQuery(userQueryKeys.current());
-
-  useEffect(() => {
-    const fetchDashboardData = async () => {
-      if (!id) {
-        return;
-      }
-      try {
-        const numericDashboardId = parseInt(id, 10);
-        const fetchedDashboardInfo = await getDashboardDetails(numericDashboardId);
-        setDashboardInfo(fetchedDashboardInfo);
-
-        const memberQuery = { page: 1, size: 10, dashboardId: numericDashboardId };
-        const fetchedMembers = await getMembers(memberQuery);
-        setMemberList(fetchedMembers);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchDashboardData();
-  }, [id]);
+  const { data: memberList } = useQuery(dashboardQueryKeys.members({ page: 1, size: 10, dashboardId: Number(id) }));
+  const { data: dashboardInfo } = useQuery(dashboardQueryKeys.detail(Number(id)));
+  const title = dashboardInfo ? dashboardInfo.title : DEFAULT_TITLE;
 
   if (!currentUser) return <div>아직 없어</div>;
-
-  const defaultTitle = "";
-  const title = dashboardInfo ? dashboardInfo.title : defaultTitle;
 
   return (
     <HeaderContainer isLanding={isLanding}>
@@ -56,6 +32,7 @@ const AuthNav = ({ isLanding }: { isLanding: boolean }) => {
         <S.NavLinks>
           {id && memberList && (
             <DashboardInfo
+              dashboardInfo={dashboardInfo}
               createdByMe={dashboardInfo?.createdByMe || false}
               dashboardId={parseInt(id, 10)}
               memberList={memberList}
